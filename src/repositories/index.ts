b242fed6b39efe71809624eab11f95188773ca8d
juanmanuel416 <jose.jimenez@ixcolombia.com>
2022-10-20 21:19:38 -0500

    contoladorserviciosycliente

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,3 +2,5 @@ export * from './usuario.repository';
 export * from './encomienda.repository';
 export * from './cliente.repository';
 export * from './servicios.repository';
+export * from './cliente.repository';
+export * from './servicios.repository';
